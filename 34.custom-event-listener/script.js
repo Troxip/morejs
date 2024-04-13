16d28f5ba2f3f5407675462b5ac31253b27a6b4a
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-04-13 20:16:25 +0200

    Promise Chain

--- 34.custom-event-listener/script.js
+++ 34.custom-event-listener/script.js
@@ -34,18 +34,70 @@
 //     console.log(err);
 //   });
 
-function isEvenNumber(number, cb) {
-  if (number % 2 === 0) {
-    cb(null, `The number ${number} is even`);
-  } else {
-    cb(new Error(`The number ${number} is not even`));
-  }
+// function isEvenNumber(number, cb) {
+//   if (number % 2 === 0) {
+//     cb(null, `The number ${number} is even`);
+//   } else {
+//     cb(new Error(`The number ${number} is not even`));
+//   }
+// }
+
+// isEvenNumber(3, (err, result) => {
+//   if (err) {
+//     console.log(err.message);
+//   } else {
+//     console.log(result);
+//   }
+// });
+
+// function isEvenNumPromise(number) {
+//   return new Promise((resolve, reject) => {
+//     if (number % 2 === 0) {
+//       resolve(`Number ${number} is even`);
+//     } else {
+//       reject(`Number ${number} is not even`);
+//     }
+//   });
+// }
+
+// isEvenNumPromise(3)
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+function stepOnePromise() {
+  return new Promise((resolve) => {
+    resolve("No");
+  });
+}
+function stepTwoPromise(result) {
+  return new Promise((resolve, reject) => {
+    if (result === "Yes") {
+      reject("You are banned");
+    } else {
+      resolve("Step Two completed");
+    }
+  });
+}
+function stepThreePromise() {
+  return new Promise((resolve) => {
+    resolve("Step Three completed");
+  });
 }
 
-isEvenNumber(3, (err, result) => {
-  if (err) {
-    console.log(err.message);
-  } else {
-    console.log(result);
-  }
-});
+stepOnePromise()
+  .then((resultFromPromiseOne) => {
+    console.log(resultFromPromiseOne);
+    return stepTwoPromise(resultFromPromiseOne);
+  })
+  .then((resultFromPromiseTwo) => {
+    console.log(resultFromPromiseTwo);
+    return stepThreePromise();
+  })
+  .then((resultFromPromiseThree) => {
+    console.log(resultFromPromiseThree);
+  })
+  .catch((err) => console.log(err));

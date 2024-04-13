3e6fc8234fc28ba21cbe15cd080a481ae6d61182
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-04-13 18:03:57 +0200

    Promise

--- 34.custom-event-listener/script.js
+++ 34.custom-event-listener/script.js
@@ -1,18 +1,51 @@
 //Creation
-function simpleCallback(success, cb) {
-  console.log("Callback Fn Called");
-  if (success) {
-    cb(null, "The operation was successful");
+// function simpleCallback1(success, cb) {
+//   console.log("Callback Fn Called");
+//   if (success) {
+//     cb(null, "The operation was successful");
+//   } else {
+//     cb("The operation failed", null);
+//   }
+// }
+
+// simpleCallback1(true, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(result);
+//   }
+//   console.log("done");
+// });
+
+// const simplePromise = new Promise((resolve, rejected) => {
+//   const success = true;
+//   if (success) {
+//     resolve("Operation was successful");
+//   } else {
+//     rejected("Operation failed!");
+//   }
+// });
+
+// simplePromise
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+function isEvenNumber(number, cb) {
+  if (number % 2 === 0) {
+    cb(null, `The number ${number} is even`);
   } else {
-    cb("The operation failed", null);
+    cb(new Error(`The number ${number} is not even`));
   }
 }
 
-simpleCallback(true, (err, result) => {
+isEvenNumber(3, (err, result) => {
   if (err) {
-    console.log(err);
+    console.log(err.message);
   } else {
     console.log(result);
   }
-  console.log("done");
 });
